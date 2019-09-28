254c9aacaec5d3c5efb7584829cddc1e6bfd6b0a
Lord Jerry <lovinjerry004@gmail.com>
2019-09-28 16:35:06 -0700

    tried implementing parsing csv

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use lexer::{ Lexer };
 use parser::{ Parser };
 
 fn main() {
-   let tokens = Lexer::new("name, age, 1, \"you\", cars
-    ;".to_string()).lex();
+   let tokens = Lexer::new(
+    "name, age, 1, \"you\", cars,
+     jerry, 18, hey, yes , 20".to_string()).lex();
     // println!("{:?}", tokens);
-    Parser::new(tokens).parse_tokens();
+    Parser::new(tokens).parse_all();
 }
  
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,4 +1,5 @@
 use lexer::kinds::{TokenKind, Token};
+use std::collections::HashMap;
 
 #[allow(dead_code)]
 pub struct Parser {
@@ -6,6 +7,7 @@ pub struct Parser {
     tokens: Vec<Result<Token, Token>>,
     // Parser Position
     position: usize,
+    header: Vec<String>,
 }
 
 impl Parser {
@@ -13,6 +15,7 @@ impl Parser {
         Parser{
             tokens,
             position: 0,
+            header: vec![],
         }
     }
 
@@ -27,11 +30,11 @@ impl Parser {
     }
 
     // return token in current parser position
-    fn peek_token(&self) -> Option<&Result<Token, Token>> {
+    fn peek_token(&self) -> Option<&Token> {
 
         // check if lexer position hasn't exceeded code length
         if self.is_bound() {
-            return Some(&self.tokens[self.position]);
+            return Some(self.tokens[self.position].as_ref().unwrap());
         }
 
         None
@@ -49,9 +52,75 @@ impl Parser {
         &self.tokens[self.position - 1]
     }
 
-    pub fn parse_tokens(&mut self) {
+    fn parse_header(&mut self) {
+        let mut header = vec![];
+        let mut temp;
+
+        // get CSV headers
+        println!("{:?}  test", self.position);
+        while self.is_bound() && (self.peek_token().unwrap().kind != TokenKind::Newline) {
+            temp = &self.tokens[self.position].as_ref().unwrap().token;
+            let kind = &self.tokens[self.position].as_ref().unwrap().kind;
+            self.position += 1;
+            println!("{:?}", self.position);
+
+            if kind == &TokenKind::Identifier {
+                header.push(temp.to_string());
+            }
+        }
+
+        if self.peek_token().unwrap().kind == TokenKind::Newline {
+            self.eat_token();
+        }
+
+        println!("{:?}", header);
+        self.header = header;
+
+        self.parse_body();
+
+    }
+
+    fn parse_body(&mut self) {
+        let header_count = self.header.len();
+        let mut count = 0;
+        let mut group = HashMap::new();
+        let mut body = vec![];
+        print!("11");
+
+        while self.is_bound() && (self.peek_token().unwrap().kind != TokenKind::Newline) {
+            let temp = &self.tokens[self.position].as_ref().unwrap().token;
+            let kind = &self.tokens[self.position].as_ref().unwrap().kind;
+            self.position += 1;
+
+            if body.len() >= self.header.len() {
+                panic!("error expected newline but got {:?}", temp);
+            } else if kind == &TokenKind::Identifier {
+                if count < header_count {
+
+                   body.push(group.insert(
+                        &self.header[count],
+                        temp.to_string(),
+                    ));
+                    count += 1;
+                }
+            }
+        }
+
+        if self.peek_token().unwrap().kind == TokenKind::Newline {
+            self.eat_token();
+        }
+
+        println!("{:?}", body);
+
+
+    }
+
+    pub fn parse_all(&mut self) {
+        println!("{:?}",self.tokens);
         while self.is_bound() {
-            println!("{:?}", self.eat_token())
+            // println!("{:?}", self.eat_token());
+            self.parse_header();
+            // self.parse_body();
         }
      }
 
