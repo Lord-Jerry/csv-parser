fdb703675b07d8285ca737af950d8319641739f7
Lord Jerry <lovinjerry004@gmail.com>
2020-01-12 21:01:39 +0100

    fix bug preventing parser from working correctly

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use lexer::{ Lexer };
-use parser::{ Parser };
+use lexer::Lexer;
+use parser::Parser;
 
 fn main() {
-   let tokens = Lexer::new(
-    "name, age, 1, \"you\", cars,
-     jerry, 18, hey, yes , 20".to_string()).lex();
+    let tokens = Lexer::new(
+        "name, age, location,
+     jerry, 18, lagos, yes,
+     lord, 20, ekiti, no,
+     jade, 20, akure, no,"
+            .to_string(),
+    )
+    .lex();
     // println!("{:?}", tokens);
     Parser::new(tokens).parse_all();
 }
- 
